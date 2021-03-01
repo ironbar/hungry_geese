5ce9e98834ebf31a30042f193ae8a279e59afac4
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-01 12:31:50 +0100

    Added reward function

--- hungry_geese/state.py
+++ hungry_geese/state.py
@@ -9,12 +9,15 @@ class GameState():
         self.history = []
         self.boards = []
         self.features = []
+        self.rewards = []
         self.configuration = None
 
     def update(self, observation, configuration):
         """
         Saves the observation to history and returns the state of the game
         """
+        if self.history:
+            self.rewards.append(get_reward(observation, self.history[-1], configuration))
         self.history.append(observation)
         if self.configuration is None:
             self.configuration = configuration
@@ -34,6 +37,7 @@ class GameState():
         self.history = []
         self.boards = []
         self.features = []
+        self.rewards = []
         self.configuration = None
 
     def _compute_features(self, observation):
@@ -62,3 +66,33 @@ def get_steps_to_die(step, hunger_rate, goose_len):
 
 def get_steps_to_end(step, episode_steps):
     return episode_steps - step
+
+def get_reward(current_observation, previous_observation, configuration):
+    """ Computes the reward for the previous action"""
+    if current_observation['geese'][current_observation['index']]:
+        is_terminal_step = current_observation['step'] == configuration['episodeSteps'] -1
+        if is_terminal_step:
+            return _get_terminal_reward(current_observation, previous_observation)
+        else:
+            # Give reward if some geese has died
+            return get_n_geese_alive(previous_observation['geese']) - get_n_geese_alive(current_observation['geese'])
+    else:
+        # Then the agent has died
+        return -1
+
+def _get_terminal_reward(current_observation, previous_observation):
+    reward = get_n_geese_alive(previous_observation['geese']) - get_n_geese_alive(current_observation['geese'])
+    goose_len = len(current_observation['geese'][current_observation['index']])
+    for idx, goose in enumerate(current_observation['geese']):
+        if idx == current_observation['index']:
+            continue
+        other_goose_len = len(goose)
+        if other_goose_len: # do not add rewards for already dead geese
+            if goose_len > other_goose_len:
+                reward += 1
+            elif goose_len == other_goose_len:
+                reward += 0.5
+    return reward
+
+def get_n_geese_alive(geese):
+    return len([goose for goose in geese if goose])
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,6 +1,9 @@
 import pytest
 
-from hungry_geese.state import get_steps_to_shrink, get_steps_to_die, get_steps_to_end
+from hungry_geese.state import (
+    get_steps_to_shrink, get_steps_to_die, get_steps_to_end, get_n_geese_alive,
+    get_reward
+)
 
 @pytest.mark.parametrize('step, hunger_rate, steps_to_shrink',  [
     (0, 40, 40),
@@ -31,3 +34,26 @@ def test_steps_to_die(step, hunger_rate, goose_len, steps_to_die):
 ])
 def test_steps_to_end(step, episode_steps, steps_to_end):
     assert steps_to_end == get_steps_to_end(step, episode_steps)
+
+@pytest.mark.parametrize('geese, n',  [
+    ([[1], [1]], 2),
+    ([[1], []], 1),
+])
+def test_get_n_geese_alive(geese, n):
+    assert n == get_n_geese_alive(geese)
+
+@pytest.mark.parametrize("current_observation, previous_observation, configuration, reward",[
+    ({'geese': [[1], [2], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 200}, 0),
+    ({'geese': [[1], [2], [3], []], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 200}, 1),
+    ({'geese': [[1], [2], [], []], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 200}, 2),
+    ({'geese': [[1], [], [], []], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 200}, 3),
+    ({'geese': [[], [2], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 200}, -1),
+    ({'geese': [[1], [2], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, 1.5),
+    ({'geese': [[1, 2], [2], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, 3),
+    ({'geese': [[1, 2], [2, 3], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, 2.5),
+    ({'geese': [[1], [2], [3], [4, 5]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, 1),
+    ({'geese': [[1], [2, 3], [3, 4], [4, 5]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, 0),
+    ({'geese': [[], [2], [3], [4]], 'index':0, 'step': 5}, {'geese': [[1], [2], [3], [4]]}, {'episodeSteps': 6}, -1),
+])
+def test_reward(current_observation, previous_observation, configuration, reward):
+    assert reward == get_reward(current_observation, previous_observation, configuration)
