3d307317a99c942b1917223c6ec1b15632b7c694
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-05 17:08:22 +0100

    More tests for epsilon agent

--- tests/agents/test_epsilon.py
+++ tests/agents/test_epsilon.py
@@ -15,4 +15,15 @@ def test_epsilon_agent_makes_legal_actions(action, epsilon):
     for previous_action, action in zip(actions[:-1], actions[1:]):
         assert action != opposite_action(previous_action)
 
-# test_epsilon_agent_random_action_probability
+@pytest.mark.parametrize('action', ['NORTH', 'WEST'])
+@pytest.mark.parametrize('epsilon, expected_action_probability', [
+    (0, 1),
+    (0.5, 0.5*(1+0.25)),
+    (1, 0.25)
+])
+def test_epsilon_agent_random_action_probability(action, epsilon, expected_action_probability, n_runs=1000):
+    agent_base = ConstantAgent(action)
+    agent = EpsilonAgent(agent_base, epsilon)
+    actions = [agent(None, None) for _ in range(n_runs)]
+    matches = sum(action == _action for _action in actions)
+    assert expected_action_probability == pytest.approx(matches/n_runs, abs=5e-2)
