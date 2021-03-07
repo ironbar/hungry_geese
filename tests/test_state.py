e4929814b117fca804984fc8c3c38cb3c96e9425
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-07 16:46:49 +0100

    More tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,8 +1,10 @@
 import pytest
+from kaggle_environments import make
 
 from hungry_geese.state import (
     get_steps_to_shrink, get_steps_to_die, get_steps_to_end, get_n_geese_alive,
-    get_reward
+    get_reward, GameState, horizontal_simmetry, vertical_simmetry, player_simmetry,
+    permutations, combine_data
 )
 
 @pytest.mark.parametrize('step, hunger_rate, steps_to_shrink',  [
@@ -58,3 +60,37 @@ def test_get_n_geese_alive(geese, n):
 ])
 def test_reward(current_observation, previous_observation, configuration, reward):
     assert reward == get_reward(current_observation, previous_observation, configuration)
+
+@pytest.fixture
+def train_data():
+    env = make('hungry_geese', configuration=dict(episodeSteps=200))
+    trainer = env.train([None, "greedy", "greedy", "greedy"])
+    configuration = env.configuration
+    state = GameState()
+
+    obs = trainer.reset()
+    done = False
+    action = 'NORTH'
+    while not done:
+        state.update(obs, configuration)
+        state.add_action(action)
+        obs, reward, done, info = trainer.step(action)
+    state.update(obs, configuration)
+    return state.prepare_data_for_training()
+
+def test_horizontal_simmetry_is_invertible(train_data):
+    inverted_data = horizontal_simmetry(horizontal_simmetry(train_data))
+    assert all((train_data[idx] == inverted_data[idx]).all() for idx in range(4))
+
+def test_vertical_simmetry_is_invertible(train_data):
+    inverted_data = vertical_simmetry(vertical_simmetry(train_data))
+    assert all((train_data[idx] == inverted_data[idx]).all() for idx in range(4))
+
+@pytest.mark.parametrize('new_positions', list(permutations([0, 1, 2])))
+def test_player_simmetry_is_invertible(train_data, new_positions):
+    inverted_data = player_simmetry(player_simmetry(train_data, [2, 1, 0]), [2, 1, 0])
+    assert all((train_data[idx] == inverted_data[idx]).all() for idx in range(4))
+
+def test_combine_data(train_data):
+    new_data = combine_data([train_data, train_data])
+    assert len(new_data[0]) == len(train_data[0])*2
