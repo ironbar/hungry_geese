d8a2fc7819c0ba1161f909991639a9fe725f6acb
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-07 18:46:55 +0100

    more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import pytest
+import tensorflow as tf
 
-from hungry_geese.utils import random_legal_action, get_timestamp
+from hungry_geese.utils import (
+    random_legal_action, get_timestamp,
+    log_to_tensorboard, log_configuration_to_tensorboard)
 
 @pytest.mark.parametrize('previous_action, ilegal_next_action', [
     ('NORTH', 'SOUTH'),
@@ -13,3 +16,11 @@ def test_random_legal_action(previous_action, ilegal_next_action):
 
 def test_get_timestamp_is_callable():
     get_timestamp()
+
+def test_log_to_tensorboard(tmpdir):
+    tensorboard_writer = tf.summary.create_file_writer(str(tmpdir))
+    log_to_tensorboard('key', 0, 0, tensorboard_writer)
+
+def test_log_configuration_to_tensorboard(tmpdir):
+    tensorboard_writer = tf.summary.create_file_writer(str(tmpdir))
+    log_configuration_to_tensorboard(dict(key=0), tensorboard_writer)
