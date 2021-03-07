db579453f4960d887212e795c8c69f61d5a1a804
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-07 18:01:01 +0100

    More tests

--- tests/agents/test_q_value.py
+++ tests/agents/test_q_value.py
@@ -16,6 +16,12 @@ class FakeModelRandom():
     def predict(self, *args, **kwargs):
         return np.random.uniform(size=(1, 4))
 
+class FakeModelOhe():
+    def predict(self, *args, **kwargs):
+        ohe = np.zeros((1, 4))
+        ohe[0, 0] = 1
+        return ohe
+
 @pytest.fixture
 def train_info():
     env = make('hungry_geese', configuration=dict(episodeSteps=200))
@@ -24,10 +30,28 @@ def train_info():
     obs = trainer.reset()
     return obs, configuration
 
-@pytest.mark.parametrize('model', [FakeModelRandom(), FakeModelUniform()])
+@pytest.mark.parametrize('model', [FakeModelRandom(), FakeModelUniform(), FakeModelOhe()])
 def test_agent_makes_legal_actions(train_info, model):
     agent = QValueAgent(model)
     agent.reset()
     actions = [agent(*train_info) for _ in range(100)]
     for previous_action, action in zip(actions[:-1], actions[1:]):
         assert action != opposite_action(previous_action)
+
+@pytest.mark.parametrize('model', [FakeModelRandom(), FakeModelUniform()])
+def test_agent_makes_random_actions(train_info, model):
+    agent = QValueAgent(model)
+    agent.reset()
+    actions = [agent(*train_info) for _ in range(1000)]
+    _, counts = np.unique(actions, return_counts=True)
+    for count in counts:
+        assert count > 200
+        assert count < 300
+
+@pytest.mark.parametrize('model', [FakeModelOhe()])
+def test_agent_makes_constant_actions(train_info, model):
+    agent = QValueAgent(model)
+    agent.reset()
+    actions = [agent(*train_info) for _ in range(100)]
+    _, counts = np.unique(actions, return_counts=True)
+    assert counts[0] == 100
