0cfbbb0375fb4a1cfa78da55dea98ef9a2548d31
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-27 11:11:34 +0100

    comparison between epsilon greedy and softmax

---
+++ data/agents/quantum/softmax_24_quantum.py
@@ -0,0 +1,14 @@
+import os
+from hungry_geese.agents import SoftmaxAgent, EpsilonAgent
+
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+import tensorflow as tf
+
+model_path = '/mnt/hdd0/MEGA/AI/22 Kaggle/hungry_geese/data/agents/quantum/best_model.h5'
+model = tf.keras.models.load_model(model_path, compile=False)
+model = tf.keras.models.Model(inputs=model.inputs[:2], outputs=model.layers[-3].output)
+softmax_agent = SoftmaxAgent(model, scale=24)
+
+def agent(obs, config):
+    return softmax_agent(obs, config)
