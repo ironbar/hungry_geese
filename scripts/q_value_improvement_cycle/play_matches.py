92c222d518243318507853d1b3431e9b13393260
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-03-28 18:18:16 +0200

    Created script to play matches in parallel

---
+++ scripts/q_value_improvement_cycle/play_matches.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import argparse
+import numpy as np
+import tempfile
+from tqdm import tqdm
+import logging
+from functools import partial
+import time
+import random
+from concurrent.futures import ProcessPoolExecutor
+from kaggle_environments import make
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
+
+logger = logging.getLogger(__name__)
+
+from hungry_geese.evaluation import play_matches_in_parallel, monitor_progress
+from hungry_geese.elo import EloRanking
+from hungry_geese.definitions import INITIAL_ELO_RANKING, AGENT_TO_SCRIPT
+from hungry_geese.utils import log_ram_usage
+from hungry_geese.state import GameState, combine_data
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    simple_model_softmax_policy_data_generation(args.model_path, args.softmax_scale, args.output, args.n_matches, args.reward_name)
+
+
+def simple_model_softmax_policy_data_generation(model_path, softmax_scale, output_path, n_matches, reward_name):
+    template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'play_template.py')
+    with open(template_path, 'r') as f:
+        text = f.read()
+    text = text.replace('model_path', os.path.realpath(model_path))
+    text = text.replace('softmax_scale', str(softmax_scale))
+    with tempfile.TemporaryDirectory() as tempdir:
+        agent_filepath = os.path.join(tempdir, 'agent.py')
+        with open(agent_filepath, 'w') as f:
+            f.write(text)
+
+        matches = play_matches_in_parallel(agents=[agent_filepath]*4, n_matches=n_matches)
+        create_train_data(matches, reward_name, output_path)
+
+
+def play_matches_in_parallel(agents, max_workers=20, n_matches=1000, running_on_notebook=False):
+    """
+    Plays n_matches in parallel using ProcessPoolExecutor
+
+    Parameters
+    -----------
+    agents : list
+        List of the agents that we will use for playing
+    """
+    log_ram_usage()
+    with ProcessPoolExecutor(max_workers=max_workers) as pool:
+        matches_results = []
+        submits = []
+        for i in range(n_matches):
+            if isinstance(agents, list):
+                submits.append(pool.submit(play_game, agents=agents))
+            elif callable(agents):
+                submits.append(pool.submit(play_game, agents=agents()))
+            else:
+                raise TypeError(type(agents))
+        monitor_progress(submits, running_on_notebook)
+        matches_results = [submit.result() for submit in submits]
+    log_ram_usage()
+    return matches_results
+
+
+def play_game(agents):
+    env = make("hungry_geese")
+    return env.run(agents=agents)
+
+
+def create_train_data(matches_results, reward_name, output_path, agent_idx_range=None):
+    """
+    Creates train data without any simmetry
+
+    Parameters
+    ----------
+    saved_games_paths : list of str
+        Path to the games that we want to use
+    reward_name : str
+        Name of the reward function that we want to use
+    output_path : str
+        Path to the file were we are going to save the results
+    max_workers : int
+    agent_idx_range : list of int
+        Idx of the agents we want to use for collecting data, if None all the agents
+        will be used
+    """
+    env = make("hungry_geese")
+    conf = env.configuration
+
+    state = GameState(reward_name=reward_name)
+    train_data = []
+    agent_idx_range = agent_idx_range or list(range(4))
+
+
+    for _ in tqdm(range(len(matches_results)), desc='Creating game data'):
+        match = matches_results[0]
+        for idx in agent_idx_range:
+            state.reset()
+            for step_idx, step in enumerate(match):
+                observation = step[0]['observation'].copy()
+                observation['index'] = idx
+                state.update(observation, conf)
+                if step_idx:
+                    state.add_action(step[idx]['action'])
+                if not observation['geese'][idx]:
+                    break
+            train_data.append(state.prepare_data_for_training())
+        del matches_results[0]
+
+    log_ram_usage()
+    logger.info('Going to combine the data')
+    train_data = combine_data(train_data)
+    log_ram_usage()
+    logger.info('Size of the boards is %.1f GB (%s [%.1f GB])' % (
+        train_data[0].nbytes/1e9,
+        str([round(data.nbytes/1e9, 1) for data in train_data]),
+        np.sum([data.nbytes/1e9 for data in train_data])))
+    output_path = os.path.realpath(output_path)
+    logger.info('Saving data on: %s' % output_path)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    np.savez_compressed(output_path, boards=train_data[0], features=train_data[1], actions=train_data[2], rewards=train_data[3])
+    del state
+    del train_data
+    log_ram_usage()
+
+
+def parse_args(args):
+    epilog = """
+    """
+    parser = argparse.ArgumentParser(
+        description='Play matches in parallel using a model',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('model_path', help='Path to the keras model', type=str)
+    parser.add_argument('softmax_scale', help='Scale of the softmax policy', type=float)
+    parser.add_argument('reward_name', help='Name of the reward we want to use', type=str)
+    parser.add_argument('output', help='Path to the npz file that will be created with the matches', type=str)
+    parser.add_argument('--n_matches', help='Number of matches that we want to run', type=int, default=500)
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/q_value_improvement_cycle/q_value_pretrain_generator_enqueuer.py
@@ -0,0 +1,180 @@
+import os
+import sys
+import argparse
+import numpy as np
+import yaml
+from tqdm import tqdm
+import logging
+from functools import partial
+import time
+import random
+
+from kaggle_environments import make
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+from hungry_geese.model import simple_model, create_model_for_training
+from hungry_geese.callbacks import (
+    LogEpochTime, LogLearningRate, LogRAM, LogCPU, LogGPU, LogETA, GarbageCollector
+)
+from hungry_geese.utils import log_ram_usage, configure_logging
+from hungry_geese.state import vertical_simmetry, horizontal_simmetry, player_simmetry
+
+logger = logging.getLogger(__name__)
+
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    train_q_value(args)
+
+def train_q_value(args):
+    configure_logging()
+    with open(args.config_path, 'r') as f:
+        conf = yaml.safe_load(f)
+    model_dir = os.path.dirname(os.path.realpath(args.config_path))
+
+    train_data = load_data(conf['train'])
+    val_data = load_data(conf['val'])
+
+    log_ram_usage()
+    if 'pretrained_model' in conf:
+        logger.info('loading pretrained model: %s' % conf['pretrained_model'])
+        training_model = tf.keras.models.load_model(os.path.join(model_dir, conf['pretrained_model']))
+    else:
+        logger.info('creating model')
+        model = simple_model(**conf['model_params'])
+        model.summary()
+        training_model = create_model_for_training(model)
+    optimizer = tf.keras.optimizers.get(conf.get('optimizer', 'Adam'))
+    optimizer.learning_rate = conf.get('learning_rate', 1e-3)
+    training_model.compile(optimizer, loss='mean_squared_error')
+    log_ram_usage()
+
+    callbacks = create_callbacks(conf['callbacks'], model_dir, conf['fit_params']['epochs'])
+    log_ram_usage()
+
+
+    train_generator = generator(train_data, conf['train_batch_size'], data_augmentation=conf['data_augmentation'])
+    val_generator = generator(val_data, conf['val_batch_size'])
+
+
+    # Enqueuer
+    generator_params = dict(use_multiprocessing=False)
+    train_generator = tf.keras.utils.GeneratorEnqueuer(train_generator, **generator_params)
+    val_generator = tf.keras.utils.GeneratorEnqueuer(val_generator, **generator_params)
+    start_params = dict(workers=1, max_queue_size=10)
+    train_generator.start(**start_params)
+    val_generator.start(**start_params)
+
+    # test sampling speed
+    add_default_steps_if_missing(conf, train_data, val_data)
+    sampling_speed_generator = train_generator.get()
+    t0 = time.time()
+    for _ in tqdm(range(conf['fit_params']['steps_per_epoch']), desc='sampling speed test'):
+        next(sampling_speed_generator)
+    logger.info('It takes %.1f seconds to sample enough data for an epoch' % (time.time() - t0))
+
+    training_model.fit(
+        x=train_generator.get(), validation_data=val_generator.get(),
+        callbacks=callbacks, **conf['fit_params'])
+    train_generator.stop()
+    val_generator.stop()
+
+
+def add_default_steps_if_missing(conf, train_data, val_data):
+    conf['fit_params']['steps_per_epoch'] = conf['fit_params'].get(
+        'steps_per_epoch', len(train_data[0])//conf['train_batch_size'])
+    conf['fit_params']['validation_steps'] = conf['fit_params'].get(
+        'validation_steps', len(val_data[0])//conf['val_batch_size'])
+    logger.info('fit_params: %s' % str(conf['fit_params']))
+
+
+def generator(train_data, batch_size, data_augmentation=False):
+    idx_range = np.arange(len(train_data[0]))
+    num_splits = len(idx_range)//batch_size
+    logger.info('Looping over the dataset will take %i steps' % num_splits)
+    while 1:
+        np.random.shuffle(idx_range)
+        for idx in range(num_splits):
+            split_idx = idx_range[idx*batch_size:(idx+1)*batch_size]
+            batch_data = [data[split_idx] for data in train_data]
+            if data_augmentation:
+                batch_data = apply_data_augmentation(batch_data)
+            x = (batch_data[0].astype(np.float32), batch_data[1], batch_data[2])
+            y = batch_data[3]
+            yield (x, y)
+
+
+def apply_data_augmentation(batch_data):
+    if random.randint(0, 1):
+        batch_data = vertical_simmetry(batch_data)
+    if random.randint(0, 1):
+        batch_data = horizontal_simmetry(batch_data)
+    if random.uniform(0, 1) > 0.16:
+        all_permutations = [(0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)]
+        new_positions = all_permutations[random.randint(0, 4)]
+        batch_data = player_simmetry(batch_data, new_positions)
+    return batch_data
+
+
+def load_data(filepath):
+    logger.info('loading %s' % filepath)
+    data = np.load(filepath)
+    output = data['boards'], data['features'], data['actions'], data['rewards'].astype(np.float32)
+    log_ram_usage()
+    logger.info('data types: %s' % str([array.dtype for array in output]))
+    logger.info('data shapes: %s' % str([array.shape for array in output]))
+    return output
+
+
+def create_callbacks(conf, model_folder, max_epochs):
+    """
+    Params
+    -------
+    conf : dict
+        Configuration of the callbacks from the train configuration file
+    """
+    logger.info('creating callbacks')
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(model_folder, 'logs'), profile_batch=0)
+    tensorboard_callback._supports_tf_logs = False
+    callbacks = [
+        LogEpochTime(),
+        LogLearningRate(),
+        LogRAM(),
+        LogCPU(),
+        LogGPU(),
+        LogETA(max_epochs),
+        tensorboard_callback,
+        GarbageCollector(),
+    ]
+    if 'ReduceLROnPlateau' in conf:
+        callbacks.append(ReduceLROnPlateau(**conf['ReduceLROnPlateau']))
+    if 'EarlyStopping' in conf:
+        logger.info('Adding EarlyStopping callback')
+        callbacks.append(EarlyStopping(**conf['EarlyStopping']))
+    for key in conf:
+        if 'ModelCheckpoint' in key and not 'PeriodModelCheckpoint' in key:
+            logger.info('Adding %s callback' % key)
+            conf[key]['filepath'] = os.path.join(model_folder, conf[key]['filename'])
+            conf[key].pop('filename')
+            callbacks.append(ModelCheckpoint(**conf[key]))
+    return callbacks
+
+
+
+def parse_args(args):
+    epilog = """
+    """
+    parser = argparse.ArgumentParser(
+        description='Train a model that learns q value function',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('config_path', help='Path to yaml file with training configuration')
+    return parser.parse_args(args)
+
+if __name__ == '__main__':
+    main()
