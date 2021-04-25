aaab67dd1f3ce37281ed6ffb51babb0c1c608af1
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-04-25 20:09:46 +0200

    started to modify q value agent to work with 3 actions

--- hungry_geese/agents/q_value.py
+++ hungry_geese/agents/q_value.py
@@ -9,21 +9,22 @@ class QValueAgent():
     def __init__(self, model):
         self.model = model
         self.state = GameState()
-        self.previous_action = None
+        self.previous_action = 'NORTH'
         self.q_values = []
 
     def __call__(self, observation, configuration):
+        # TODO: how to deal with the first action
         board, features = self.state.update(observation, configuration)
         q_value = np.array(self.model.predict_step([np.expand_dims(board, axis=0), np.expand_dims(features, axis=0)])[0])
         self.q_values.append(q_value.copy())
-        action = ACTIONS[self.select_action(q_value, observation, configuration)]
+        action = self.select_action(q_value, observation, configuration)
         self.previous_action = action
         self.state.add_action(action)
         return action
 
     def reset(self):
         self.state.reset()
-        self.previous_action = None
+        self.previous_action = 'NORTH'
         self.q_values = []
 
     def update_previous_action(self, previous_action):
@@ -32,10 +33,10 @@ class QValueAgent():
         self.state.update_last_action(previous_action)
 
     def select_action(self, q_value, observation, configuration):
-        q_value += np.random.uniform(0, 1e-3, len(q_value))
-        if self.previous_action is not None:
-            q_value[ACTION_TO_IDX[opposite_action(self.previous_action)]] -= 1e6
-        return np.argmax(q_value)
+        q_value += np.random.uniform(0, 1e-5, len(q_value))
+        action_idx = np.argmax(q_value)
+        action = ACTIONS[(action_idx - 1 + ACTION_TO_IDX[self.previous_action])%len(ACTIONS)]
+        return action
 
 
 class QValueSafeAgent(QValueAgent):
@@ -46,9 +47,9 @@ class QValueSafeAgent(QValueAgent):
         q_value += np.random.uniform(0, 1e-5, len(q_value))
         certain_death_mask = get_certain_death_mask(observation, configuration)
         q_value -= np.floor(certain_death_mask)*1e2
-        if self.previous_action is not None:
-            q_value[ACTION_TO_IDX[opposite_action(self.previous_action)]] -= 1e6
-        return np.argmax(q_value)
+        action_idx = np.argmax(q_value)
+        action = ACTIONS[(action_idx - 1 + ACTION_TO_IDX[self.previous_action])%len(ACTIONS)]
+        return action
 
 
 class QValueSafeMultiAgent(QValueSafeAgent):
@@ -66,7 +67,7 @@ class QValueSafeMultiAgent(QValueSafeAgent):
         model_input = [np.expand_dims(board, axis=0), np.expand_dims(features, axis=0)]
         q_value = np.mean([model.predict_step(model_input)[0] for model in self.models], axis=0)
         self.q_values.append(q_value.copy())
-        action = ACTIONS[self.select_action(q_value, observation, configuration)]
+        action = self.select_action(q_value, observation, configuration)
         self.previous_action = action
         self.state.add_action(action)
         return action
