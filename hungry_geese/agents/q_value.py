80ec5dcdeaf90f68a552897cc10ff00e5cf90cec
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-04-29 18:39:41 +0200

    modified q value agent and added tests to verify behaviour

--- hungry_geese/agents/q_value.py
+++ hungry_geese/agents/q_value.py
@@ -73,16 +73,27 @@ class QValueAgent():
 
 class QValueSafeAgent(QValueAgent):
     """
-    This version does not move into death positions
+    This version does not take risks if possible
     """
     def select_action(self, q_value, observation, configuration):
         q_value += np.random.uniform(0, 1e-5, len(q_value))
         certain_death_mask = get_certain_death_mask(observation, configuration)
         certain_death_mask = adapt_mask_to_3d_action(certain_death_mask, self.previous_action)
-        q_value -= np.floor(certain_death_mask)*1e2
-        action_idx = np.argmax(q_value)
-        action = get_action_from_relative_movement(action_idx, self.previous_action)
-        return action
+
+        safe_movements = np.arange(len(certain_death_mask))[certain_death_mask == 0]
+        if safe_movements.size:
+            return self._select_between_available_actions(safe_movements, q_value)
+
+        risky_movements = np.arange(len(certain_death_mask))[certain_death_mask < 1]
+        if risky_movements.size:
+            return self._select_between_available_actions(risky_movements, q_value)
+
+        return self._select_between_available_actions(np.arange(len(certain_death_mask)), q_value)
+
+    def _select_between_available_actions(self, available_actions, q_value):
+        q_value = q_value[available_actions]
+        action_idx = available_actions[np.argmax(q_value)]
+        return get_action_from_relative_movement(action_idx, self.previous_action)
 
 
 class QValueSafeMultiAgent(QValueSafeAgent):
--- tests/agents/test_q_value.py
+++ tests/agents/test_q_value.py
@@ -91,3 +91,33 @@ def test_QValueSafeMultiAgent_play():
     match = env.run(agents)
     final_status = [agent_info['status'] for agent_info in match[-1]]
     assert 'ERROR' not in final_status
+
+@pytest.mark.parametrize('n_runs', [100])
+@pytest.mark.parametrize('observation, risky_actions', [
+    ({'index': 0, 'geese': [[36, 47], [26, 27]]}, ['NORTH', 'EAST']),
+    ({'index': 0, 'geese': [[36, 47], [14, 3]]}, ['NORTH']),
+    ({'index': 0, 'geese': [[36, 47], [24, 23]]}, ['NORTH', 'WEST']),
+])
+def test_QValueSafeAgent_does_not_take_risky_actions_if_possible(observation, risky_actions, n_runs):
+    agent = QValueSafeAgent(None)
+    configuration = dict(columns=11, rows=7)
+    q_value = np.ones(3)
+    actions = []
+    for _ in range(n_runs):
+        actions.append(agent.select_action(q_value.copy(), observation, configuration))
+    assert all(risky_action not in actions for risky_action in risky_actions)
+
+@pytest.mark.parametrize('n_runs', [100])
+@pytest.mark.parametrize('observation, risky_actions', [
+    ({'index': 0, 'geese': [[36, 47], [26, 27], [24, 35, 46]]}, ['NORTH', 'EAST']),
+    ({'index': 0, 'geese': [[36, 47], [14, 3], [24, 35, 46], [26, 37, 48]]}, ['NORTH']),
+    ({'index': 0, 'geese': [[36, 47], [24, 23], [26, 37, 48]]}, ['NORTH', 'WEST']),
+])
+def test_QValueSafeAgent_does_take_risky_actions_if_necessary(observation, risky_actions, n_runs):
+    agent = QValueSafeAgent(None)
+    configuration = dict(columns=11, rows=7)
+    q_value = np.ones(3)
+    actions = []
+    for _ in range(n_runs):
+        actions.append(agent.select_action(q_value.copy(), observation, configuration))
+    assert all(risky_action in actions for risky_action in risky_actions)
