04c0f82b0d4f9c521d05cca39be32833d352c7be
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-04-30 19:20:19 +0200

    updated agent templates because now the model is saved as it is

--- scripts/deep_q_learning/q_value_agent_template.py
+++ scripts/deep_q_learning/q_value_agent_template.py
@@ -4,7 +4,6 @@ from hungry_geese.agents import QValueAgent
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 import tensorflow as tf
 model = tf.keras.models.load_model('model_path', compile=False)
-model = tf.keras.models.Model(inputs=model.inputs[:2], outputs=model.layers[-3].output)
 q_value_agent = QValueAgent(model)
 def agent(obs, config):
     return q_value_agent(obs, config)
