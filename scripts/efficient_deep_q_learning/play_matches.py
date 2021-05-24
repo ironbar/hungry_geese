5a3eaa5ce6ee26d10f0566ed0be165411450c3e9
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-05-24 19:11:43 +0200

    Added scripts for playing games

---
+++ scripts/efficient_deep_q_learning/play_matches.py
@@ -0,0 +1,85 @@
+import os
+import glob
+import sys
+import argparse
+import logging
+import yaml
+import subprocess
+import tensorflow as tf
+
+from hungry_geese.utils import configure_logging, log_to_tensorboard, get_timestamp
+
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    configure_logging()
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    with open(args.config_path, 'r') as f:
+        conf = yaml.safe_load(f)
+
+    model_dir = os.path.dirname(os.path.realpath(args.config_path))
+    tensorboard_writer = tf.summary.create_file_writer(os.path.join(model_dir, 'logs'))
+
+    while 1:
+        model_path = get_latest_model_path(model_dir)
+        epoch = get_epoch_from_model_path(model_path)
+        train_data_path = os.path.join(model_dir, 'epoch_%05d_timestamp_%s.npz' % (epoch, get_timestamp()))
+        play_results = play_matches(
+            model_path=model_path,
+            softmax_scale=conf['softmax_scale'],
+            reward_name=conf['reward'],
+            train_data_path=train_data_path,
+            n_matches=conf['n_matches_play'],
+            template_path=conf['play_template'],
+            play_against_top_n=conf.get('play_against_top_n', 0),
+            n_learning_agents=conf.get('n_learning_agents', 1),
+            n_agents_for_experience=conf['n_agents_for_experience'])
+        for key, value in play_results.items():
+            log_to_tensorboard(key, value, epoch, tensorboard_writer)
+
+
+def get_latest_model_path(model_dir):
+    model_paths = sorted(glob.glob(os.path.join(model_dir, 'epoch*.h5')))
+    return model_paths[-1]
+
+
+def get_epoch_from_model_path(model_path):
+    epoch = int(os.path.splitext(os.path.basename(model_path))[0].split('epoch_')[-1])
+    return epoch
+
+
+def play_matches(model_path, softmax_scale, reward_name, train_data_path, n_matches,
+                 template_path, play_against_top_n, n_learning_agents, n_agents_for_experience):
+    logger.info('Playing matches in parallel with: %s' % model_path)
+    command = 'python "%s" "%s" %.1f %s "%s" "%s" --n_matches %i --play_against_top_n %i' % (
+        '/mnt/hdd0/MEGA/AI/22 Kaggle/hungry_geese/scripts/efficient_deep_q_learning/play_matches_one_round.py',
+        model_path, softmax_scale, reward_name, train_data_path, template_path, n_matches, play_against_top_n)
+    command += ' --n_learning_agents %i --n_agents_for_experience %i' % (n_learning_agents, n_agents_for_experience)
+    output = subprocess.getoutput(command).split('\n')[-1]
+    print(output)
+    return eval(output)
+
+
+def parse_args(args):
+    epilog = """
+    """
+    description = """
+    Play matches with the latests available model and saves metrics to tensorboard
+    """
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('config_path', help='Path to yaml file with training configuration')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/efficient_deep_q_learning/play_matches_one_round.py
@@ -0,0 +1,286 @@
+import os
+import sys
+import argparse
+import numpy as np
+import tempfile
+from tqdm import tqdm
+import logging
+from functools import partial
+import time
+import random
+import yaml
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor
+from kaggle_environments import make
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
+
+logger = logging.getLogger(__name__)
+
+from hungry_geese.evaluation import play_matches_in_parallel, monitor_progress
+from hungry_geese.elo import EloRanking
+from hungry_geese.definitions import INITIAL_ELO_RANKING, AGENT_TO_SCRIPT
+from hungry_geese.utils import log_ram_usage
+from hungry_geese.state import GameState, combine_data
+from hungry_geese.utils import configure_logging
+from hungry_geese.heuristic import get_certain_death_mask, adapt_mask_to_3d_action
+from hungry_geese.reward import get_death_reward_from_name
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    configure_logging()
+    simple_model_softmax_policy_data_generation(
+        args.model_path, args.softmax_scale, args.output, args.n_matches, args.reward_name,
+        template_path=args.template_path, play_against_top_n=args.play_against_top_n,
+        n_learning_agents=args.n_learning_agents, n_agents_for_experience=args.n_agents_for_experience)
+
+
+def simple_model_softmax_policy_data_generation(model_path, softmax_scale, output_path,
+                                                n_matches, reward_name, template_path,
+                                                play_against_top_n, n_learning_agents,
+                                                n_agents_for_experience):
+    # template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'play_template.py')
+    with open(template_path, 'r') as f:
+        text = f.read()
+    text = text.replace('model_path', os.path.realpath(model_path))
+    text = text.replace('softmax_scale', str(softmax_scale))
+    with tempfile.TemporaryDirectory() as tempdir:
+        agent_filepath = os.path.join(tempdir, 'agent.py')
+        with open(agent_filepath, 'w') as f:
+            f.write(text)
+        if not play_against_top_n:
+            matches = play_matches_in_parallel(agents=[agent_filepath]*4, n_matches=n_matches)
+            create_train_data(matches, reward_name, output_path)
+        else:
+            script_path = os.path.realpath(__file__)
+            repo_path = os.path.dirname(os.path.dirname(os.path.dirname(script_path)))
+            current_elo_ranking = pd.read_csv(os.path.join(repo_path, 'data/elo_ranking.csv'), index_col='model')
+            with open(os.path.join(repo_path, 'data/agents.yml'), 'r') as f:
+                agents = yaml.safe_load(f)
+            adversary_names = current_elo_ranking.index.values[:play_against_top_n].tolist()
+            # logger.info('Playing against: %s' % str(adversary_names))
+            # print('Playing against: %s' % str(adversary_names))
+            adversaries = [agents[name] for name in adversary_names]
+            sample_func = lambda: [agent_filepath]*n_learning_agents \
+                + np.random.choice(adversaries, size=(4 - n_learning_agents)).tolist()
+            matches = play_matches_in_parallel(agents=sample_func, n_matches=n_matches)
+            metrics = gather_metrics_from_matches(matches)
+            create_train_data(matches, reward_name, output_path, agent_idx_range=list(range(n_agents_for_experience)))
+            print(metrics) # for capturing this print with subprocess
+
+
+def play_matches_in_parallel(agents, max_workers=20, n_matches=1000, running_on_notebook=False):
+    """
+    Plays n_matches in parallel using ProcessPoolExecutor
+
+    Parameters
+    -----------
+    agents : list
+        List of the agents that we will use for playing
+    """
+    log_ram_usage()
+    with ProcessPoolExecutor(max_workers=max_workers) as pool:
+        matches_results = []
+        submits = []
+        for i in range(n_matches):
+            if isinstance(agents, list):
+                submits.append(pool.submit(play_game, agents=agents))
+            elif callable(agents):
+                submits.append(pool.submit(play_game, agents=agents()))
+            else:
+                raise TypeError(type(agents))
+        monitor_progress(submits, running_on_notebook)
+        matches_results = [submit.result() for submit in submits]
+    log_ram_usage()
+    return matches_results
+
+
+def play_game(agents):
+    env = make("hungry_geese")
+    return env.run(agents=agents)
+
+
+def create_train_data(matches_results, reward_name, output_path, agent_idx_range=None):
+    """
+    Creates train data without any simmetry
+
+    Parameters
+    ----------
+    saved_games_paths : list of str
+        Path to the games that we want to use
+    reward_name : str
+        Name of the reward function that we want to use
+    output_path : str
+        Path to the file were we are going to save the results
+    max_workers : int
+    agent_idx_range : list of int
+        Idx of the agents we want to use for collecting data, if None all the agents
+        will be used
+    """
+    env = make("hungry_geese")
+    conf = env.configuration
+
+    state = GameState(reward_name=reward_name, apply_reward_acumulation=False)
+    train_data = []
+    agent_idx_range = agent_idx_range or list(range(4))
+
+
+    for _ in tqdm(range(len(matches_results)), desc='Creating game data'):
+        match = matches_results[0]
+        # logger.debug('Match steps: %i' % len(match))
+        for agent_idx in agent_idx_range:
+            first_action = match[1][agent_idx]['action']
+            if first_action == 'SOUTH':
+                state.reset(previous_action='SOUTH')
+            else:
+                state.reset()
+            certain_death_masks = []
+
+            for step_idx, step in enumerate(match):
+                if step_idx:
+                    action = step[agent_idx]['action']
+                    state.add_action(action)
+                observation = step[0]['observation'].copy()
+                observation['index'] = agent_idx
+                state.update(observation, conf)
+
+                if not observation['geese'][agent_idx]:
+                    break
+                certain_death_mask = get_certain_death_mask(observation, conf)
+                certain_death_mask = adapt_mask_to_3d_action(certain_death_mask, state.get_last_action())
+                certain_death_mask = np.floor(certain_death_mask)
+                certain_death_masks.append(certain_death_mask)
+
+            data = state.prepare_data_for_training()
+            # create rewards for taken actions
+            rewards = np.expand_dims(data[-1], axis=1)
+            ohe_actions = data[-2]
+            rewards = rewards*ohe_actions
+            # add certain death reward
+            certain_death_masks = np.array(certain_death_masks)[:len(rewards)]
+            death_reward = get_death_reward_from_name(reward_name)
+            rewards += np.clip(certain_death_masks - ohe_actions, 0, 1)*death_reward
+            # is_not_terminal
+            is_not_terminal = np.ones_like(rewards)
+            is_not_terminal -= certain_death_masks
+            is_not_terminal[-1] = 0
+            # mask for training
+            training_mask = np.zeros_like(rewards)
+            training_mask += ohe_actions
+            training_mask += certain_death_masks
+            training_mask = np.clip(training_mask, 0, 1)
+
+            train_data.append(data[:2] + [rewards, is_not_terminal, training_mask])
+            # logger.debug('Data shapes %s' % str([data.shape for data in train_data[-1]]))
+        del matches_results[0]
+
+    log_ram_usage()
+    logger.info('Going to combine the data')
+    train_data = combine_data(train_data)
+    log_ram_usage()
+    logger.info('Size of the boards is %.1f GB (%s [%.1f GB])' % (
+        train_data[0].nbytes/1e9,
+        str([round(data.nbytes/1e9, 1) for data in train_data]),
+        np.sum([data.nbytes/1e9 for data in train_data])))
+    logger.info('Data shapes %s' % str([data.shape for data in train_data]))
+    output_path = os.path.realpath(output_path)
+    logger.info('Saving data on: %s' % output_path)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    np.savez_compressed(
+        output_path,
+        boards=train_data[0],
+        features=train_data[1],
+        rewards=train_data[2],
+        is_not_terminal=train_data[3],
+        training_mask=train_data[4],
+    )
+    del state
+    del train_data
+    log_ram_usage()
+
+
+def gather_metrics_from_matches(matches, agent_idx=0):
+    """
+    Computes metrics from the played matches
+
+    - mean_match_steps
+    - death ratio
+    - mean goose size
+    - position
+
+    Parameters
+    -----------
+    matches : list of match
+        List of matches with the format of hungry geese
+    """
+    match_steps, goose_size, final_score = [], [], []
+    deaths = 0
+
+    for match in tqdm(matches, desc='Gathering metrics from game data'):
+        for step_idx, step in enumerate(match):
+            goose = step[0]['observation']['geese'][agent_idx]
+            if not goose:
+                deaths += 1
+                break
+            goose_size.append(len(goose))
+        match_steps.append(step_idx)
+        rewards = [info['reward'] for info in step]
+        final_score.append(get_final_score(rewards))
+
+    final_score = np.array(final_score)
+    metrics = {
+        'mean_match_steps': np.mean(match_steps),
+        'death_ratio': deaths/len(match_steps),
+        'mean_goose_size': np.mean(goose_size),
+        'mean_final_score': np.mean(final_score),
+        'win_ratio': np.mean(final_score == 3),
+    }
+    return metrics
+
+def get_final_score(rewards, agent_idx=0):
+    """
+    Returns a value between 0 and 3, being 0 the last position and 3 the agent winning the match
+    """
+    final_score = 0
+    for idx, reward in enumerate(rewards):
+        if idx == agent_idx:
+            continue
+        if reward < rewards[agent_idx]:
+            final_score += 1
+        elif reward == rewards[agent_idx]:
+            final_score += 0.5
+    return final_score
+
+
+def parse_args(args):
+    epilog = """
+    python scripts/deep_q_learning_v2/play_matches.py /mnt/hdd0/Kaggle/hungry_geese/models/31_iterating_over_softmax_policy/01_it1_2000_lr4e4/pretrained_model.h5 8 ranking_reward_-4_4 delete.npz "/mnt/hdd0/MEGA/AI/22 Kaggle/hungry_geese/scripts/deep_q_learning_v2/softmax_safe_agent_template.py" --n_matches 50
+    """
+    parser = argparse.ArgumentParser(
+        description='Play matches in parallel using a model',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('model_path', help='Path to the keras model', type=str)
+    parser.add_argument('softmax_scale', help='Scale of the softmax policy', type=float)
+    parser.add_argument('reward_name', help='Name of the reward we want to use', type=str)
+    parser.add_argument('output', help='Path to the npz file that will be created with the matches', type=str)
+    parser.add_argument('template_path', help='Path to the python file with template for playing')
+    parser.add_argument('--n_matches', help='Number of matches that we want to run', type=int, default=500)
+    parser.add_argument('--play_against_top_n',
+                        help='If given it will play against top n agents, otherwise it will do self-play',
+                        type=int, default=0)
+    parser.add_argument('--n_learning_agents',
+                        help='How many learning agents to use, only when playing against top n agents',
+                        type=int, default=1)
+    parser.add_argument('--n_agents_for_experience',
+                        help='How many agents to use for collecting experience, only when playing against top n agents',
+                        type=int, default=1)
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
