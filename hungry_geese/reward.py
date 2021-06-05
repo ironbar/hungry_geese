2b650be80a42ff993ee8c78167669946df25dffe
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-06-05 12:17:06 +0200

    Implemented new reward terminal_kill_and_grow

--- hungry_geese/reward.py
+++ hungry_geese/reward.py
@@ -18,6 +18,19 @@ If the agent is leading then is the difference with the second.
 ## Grow and kill reward
 
 Growing gives reward and also the death of the other agents
+
+## Terminal, kill and grow reward (terminal_kill_and_grow_reward)
+
+- Terminal reward: this will be given if the agent dies or reaches the terminal state. It will be
+proportional to the ranking of the agent, f.e. 5, -5, -10, -15. This is a difference with current
+reward that was not giving reward on the terminal state. This way the agent will not find any
+difference on dying on step 190 or reaching step 200 on second position if only two goose are left.
+It will incentivate taking more risks for winning.
+- Kill reward. this is already present on current reward and I think is a good incentive. Sometimes
+the other agents will die by themselves, but if the agent learns to kill them that would be a good
+ability. We could give for example 2 for each killed agent.
+- Eat reward. I also think that encourage growing eases the learning.
+
 """
 
 import numpy as np
@@ -34,6 +47,8 @@ def get_reward(current_observation, previous_observation, configuration, reward_
         return get_grow_and_kill_reward(current_observation, previous_observation, reward_name)
     elif reward_name.startswith('just_survive'):
         return get_just_survive_reward(current_observation, reward_name)
+    elif reward_name.startswith('terminal_kill_and_grow_reward'):
+        return get_terminal_kill_and_grow_reward(current_observation, previous_observation, reward_name, configuration)
     else:
         raise KeyError(reward_name)
 
@@ -64,7 +79,7 @@ def get_cumulative_reward(rewards, reward_name):
 def get_sparse_reward(current_observation, previous_observation, configuration):
     """ Computes the sparse reward for the previous action"""
     if current_observation['geese'][current_observation['index']]:
-        is_terminal_step = current_observation['step'] == configuration['episodeSteps'] -1
+        is_terminal_step = current_observation['step'] == configuration['episodeSteps'] - 1
         if is_terminal_step:
             return _get_terminal_sparse_reward(current_observation, previous_observation)
         else:
@@ -76,6 +91,11 @@ def get_sparse_reward(current_observation, previous_observation, configuration):
 
 
 def _get_terminal_sparse_reward(current_observation, previous_observation):
+    """
+    Returns a reward between 0 and 3, where 0 means the agent is in the last position and 3 means
+    it is the winner. It gives 1 point for each smaller or death agent, and 0.5 for each agent
+    of the same size
+    """
     reward = get_n_geese_alive(previous_observation['geese']) - get_n_geese_alive(current_observation['geese'])
     goose_len = len(current_observation['geese'][current_observation['index']])
     for idx, goose in enumerate(current_observation['geese']):
@@ -157,6 +177,56 @@ def get_just_survive_reward(current_observation, reward_name):
     else: # the agent has died
         death_reward = float(reward_name.split('_')[-1])
         return death_reward
+
+
+def get_terminal_kill_and_grow_reward(current_observation, previous_observation, reward_name, configuration):
+    terminal_reward_scale, kill_reward, grow_reward = _get_terminal_kill_and_grow_reward_params_from_name(reward_name)
+    is_terminal_state = _is_goose_death(current_observation) or _is_final_state(current_observation, configuration)
+    if is_terminal_state:
+        terminal_reward = _get_terminal_sparse_reward(current_observation, previous_observation)
+        terminal_reward -= 2.5 # this only gives positive reward for winning, [-2.5, 0.5]
+        terminal_reward *= terminal_reward_scale
+        return terminal_reward
+    else:
+        reward = kill_reward * _get_killed_geese(current_observation, previous_observation)
+        reward += grow_reward * _get_goose_growth(current_observation, previous_observation)
+        return reward
+
+
+def _is_goose_death(observation):
+    geese_len = _get_geese_len(observation)
+    goose_len = geese_len[observation['index']]
+    return not goose_len
+
+
+def _get_geese_len(observation):
+    geese_len = [len(goose) for goose in observation['geese']]
+    return geese_len
+
+
+def _is_final_state(observation, configuration):
+    return observation['step'] == configuration['episodeSteps'] - 1
+
+
+def _get_killed_geese(current_observation, previous_observation):
+    """ Computes how many geese were killed between observations """
+    return get_n_geese_alive(previous_observation['geese']) - get_n_geese_alive(current_observation['geese'])
+
+
+def _get_goose_growth(current_observation, previous_observation):
+    """ Returns 1 if the goose is bigger, 0 otherwise """
+    current_len = _get_geese_len(current_observation)[current_observation['index']]
+    previous_len = _get_geese_len(previous_observation)[previous_observation['index']]
+    if current_len > previous_len:
+        return 1
+    return 0
+
+
+def _get_terminal_kill_and_grow_reward_params_from_name(reward_name):
+    """ terminal_kill_and_grow_10_2_1 """
+    terminal_reward_scale, kill_reward, grow_reward = [float(value) for value in reward_name.split('_')[-3:]]
+    return terminal_reward_scale, kill_reward, grow_reward
+
 
 def get_death_reward_from_name(reward_name):
     if reward_name == 'sparse_reward':
